d1900121e488526495bc93b8a67be1be2bae93ff
Adil-Bub <adilahmed1090@gmail.com>
2020-07-30 01:53:04 +0530

    Updates

---
+++ precache-manifest.f57fdfff91fd39bcc22919b6d3dac1f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bdc770f1e409f1cfb676ec5f37144a",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "240b6f77b4fa59d61be1",
+    "url": "/sudoku/static/css/main.7a9134c7.chunk.css"
+  },
+  {
+    "revision": "c01ad7e50dae522d0eef",
+    "url": "/sudoku/static/js/2.68415654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.68415654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "240b6f77b4fa59d61be1",
+    "url": "/sudoku/static/js/main.17cfffe1.chunk.js"
+  },
+  {
+    "revision": "8d8e892f47ac5d31712b",
+    "url": "/sudoku/static/js/runtime-main.f7976529.js"
+  },
+  {
+    "revision": "0cdf36633fee40ab0b2e5b98db541a18",
+    "url": "/sudoku/static/media/11b6769c8f2ea91b25fa0bb10bcaf4ab.0cdf3663.jpg"
+  }
+]);
